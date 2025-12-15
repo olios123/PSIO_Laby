f99291f9e8af59683d528a58e0248ec838db8ea5
olios123 <oliwier4444k@gmail.com>
2025-12-15 08:29:57 +0100

    PSIO10

---
+++ src/PSIO10/Buffer/Test.java
@@ -0,0 +1,23 @@
+package PSIO10.Buffer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Test {
+	public static void main(String[] args) {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Podaj wiersze:");
+		String line;
+		try {
+			while ((line = bf.readLine()) != null) {
+				System.out.println(line);
+			}
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+}
